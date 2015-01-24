dbdda36702a8745bcbafd1953dcb0006fafa694b
JasonChanChina <zglcx123@gmail.com>
2015-01-24 11:27:44 +0800

    移植opencv代码x64

---
+++ GrayGrabCut/stdafx.h
@@ -0,0 +1,22 @@
+#ifndef _STDAFX_H_
+#define _STDAFX_H_
+
+#include <vector>
+#include <hash_set>
+#include <string>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+
+#endif
