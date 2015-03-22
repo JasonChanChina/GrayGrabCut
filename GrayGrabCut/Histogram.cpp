051978212d44ce97eb65cf3deddd559fae8cb03f
JasonChanChina <zglcx123@gmail.com>
2015-03-22 20:42:04 +0800

    delete unused text

--- GrayGrabCut/Histogram.cpp
+++ GrayGrabCut/Histogram.cpp
@@ -7,37 +7,7 @@ Histogram::Histogram(Mat _hist)
 
 void Histogram::createHist(const Mat& img, Mat& mask)
 {
-	//int histSize = 256;
-	//float range[] = {0, 255};
-	//const float* histRange = {range};
-	//bool uniform = false;	//是否归一化/均衡化
-	//bool accumulate = false;	//是否累计之前记录
-	//calcHist( &img, 1, 0, mask, hist, 1, &histSize, &histRange, uniform, accumulate );
-
-	//int histSize = 256;
-
-	//hist.create(1, histSize, CV_32FC1); 
-	//int count = img.cols * img.rows;
-	//int maskCount = 0;
-
-	//vector<int> histCount(histSize, 0);
-	//for( int y = 0; y < img.rows; y++ )
- //   {
- //       for( int x = 0; x < img.cols; x++ )
- //       {
-	//		if(mask.at<uchar>(y,x) > 0)
-	//		{
-	//			uchar value = img.at<uchar>(y,x);
-	//			histCount[value]++;
-	//		}
-	//	}
-	//}
-
-	//for(int i = 0; i < histCount.size(); i++)
-	//{
-	//	hist.at<float>(0,i) = (float)histCount[i] / count;
-	//}
-
+	
 
 	int histSize = 256;
 
